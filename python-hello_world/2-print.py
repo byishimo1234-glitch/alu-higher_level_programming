f8ef73be1a8b8b0a2b15fd5b65bbd9e995933aca
byishimo123-glitch <j.byishimo@alustudent.com>
2025-02-26 23:30:51 -0800

    add all

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-echo -e '#!/usr/bin/python3\nprint("Programming is like building a multilingual puzzle")' > 2-print.py
+print("Programming is like building a multilingual puzzle")' > 2-print.py
