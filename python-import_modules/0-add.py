1cc438d8e2a05fd34d4775480bb7607ceb343b98
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-02 01:00:59 -0800

    add all

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add  # Import add function (only one mention of 'add_0')
 
-if __name__ == "__main__":  # Ensures the script runs only when executed directly
+if __name__ == "__main__":  # Ensures script runs only when executed directly
     a = 1  # Define a on a separate line
     b = 2  # Define b on another line
 
