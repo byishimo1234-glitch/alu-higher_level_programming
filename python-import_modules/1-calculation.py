02cad9ffee61941baa753a8cbf859b3296de93bb
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-02 04:33:24 -0800

    add all

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 
+# Define the functions from calculator_1.py
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+# Main program logic
 if __name__ == "__main__":
     a = 10
     b = 5
