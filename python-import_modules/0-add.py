27c03482934cfbe47d948cd895486e0dcdc4301b
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-02 01:03:09 -0800

    add all

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add  # Import add function (only one mention of 'add_0')
+from add_0 import add  # "add_0" appears only once in this line
 
 if __name__ == "__main__":  # Ensures script runs only when executed directly
-    a = 1  # Define a on a separate line
-    b = 2  # Define b on another line
-
-    print("{} + {} = {}".format(a, b, add(a, b)))  # Print formatted output
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))  # Correct print formatting
