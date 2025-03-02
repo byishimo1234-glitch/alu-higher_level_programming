3f569beeccc95fbf8d90518dca40b8cb569ecb47
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-02 04:23:09 -0800

    add all

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,5 +4,5 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    result = add(a, b)
+    result = add(a, b)  # This will use the fake add() function
     print("{} + {} = {}".format(a, b, result))
