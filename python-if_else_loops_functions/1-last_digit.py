afe5d7a28e6855318b28936ec123981608a9e83b
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-01 23:29:09 -0800

    add all

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10  # Get the last digit (absolute value to handle negatives)
+last_digit = abs(number) % 10  # Get the last digit
 
 if number < 0:
     last_digit = -last_digit  # Make last digit negative if number is negative
