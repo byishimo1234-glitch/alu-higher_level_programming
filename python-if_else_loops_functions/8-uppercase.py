725cc78c0254e1d2936cd2d013862784e263a435
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-02 00:23:27 -0800

    add all

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     print("".join(
-        "{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c) for c in str
+        "{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c)
+        for c in str
     ))
