7a15d1550df91dccdd33e95bf107df41a08af6f6
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-02 05:09:14 -0800

    add all

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,7 +3,6 @@
 if __name__ == "__main__":
     import sys
 
-    argv = sys.argv[1:]
-    total = sum(int(arg) for arg in argv)
+    total = sum(int(arg) for arg in sys.argv[1:])
     print(total)
 
