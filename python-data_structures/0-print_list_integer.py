690f25626af7d8146a5a2661a3998658bf39f2d4
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-05 09:25:20 -0800

    add all

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-    """Prints all integers of a list using str.format."""
     for num in my_list:
-        print("{}".format(num))
-
+        print("{:d}".format(num))
