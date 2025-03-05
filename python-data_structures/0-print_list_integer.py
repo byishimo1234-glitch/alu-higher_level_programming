53bd24d42a6f804350bc11d2c04a64497ffec7a7
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-05 09:17:44 -0800

    add all

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idx):
-    """Retrieves an element from a list like in C."""
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+def print_list_integer(my_list=[]):
+    """Prints all integers of a list using str.format."""
+    for num in my_list:
+        print("{}".format(num))
 
