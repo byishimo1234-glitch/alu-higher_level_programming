0f69337da280b1065fc23c649ca32648f6e4fc90
byishimo123-glitch <j.byishimo@alustudent.com>
2025-03-11 09:41:12 -0700

    add all

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module provides a function to read and print the content of a text file (UTF-8).
+"""
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
+
---
+++ python-input_output/README.md
@@ -0,0 +1,1 @@
+File that Write a function that reads a text file (UTF8) and prints it to stdout:
